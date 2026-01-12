7d9de11326fbbd39266494762e469d32e6c7b659
Token-Inv13 <token.invest13@gmail.com>
2026-01-12 13:30:13 +0100

    fix(android): register FirebaseAuthentication plugin in MainActivity

--- apps/web/android/app/src/main/java/com/tachesnotes/smartnotes/MainActivity.java
+++ apps/web/android/app/src/main/java/com/tachesnotes/smartnotes/MainActivity.java
@@ -2,4 +2,14 @@ package com.tachesnotes.smartnotes;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+import android.os.Bundle;
+
+import io.capawesome.capacitorjs.plugins.firebase.authentication.FirebaseAuthenticationPlugin;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    registerPlugin(FirebaseAuthenticationPlugin.class);
+    super.onCreate(savedInstanceState);
+  }
+}
