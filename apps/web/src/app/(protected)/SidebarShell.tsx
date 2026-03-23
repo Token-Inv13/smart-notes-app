8b678ca70ebf10d9693d8cc47c52640a7dd751ab
Token <token.invest13@gmail.com>
2026-03-23 16:02:44 +0100

    fix: repair SidebarShell mobile drawer conditional

--- apps/web/src/app/(protected)/SidebarShell.tsx
+++ apps/web/src/app/(protected)/SidebarShell.tsx
@@ -851,7 +851,7 @@ export default function SidebarShell({ children }: { children: React.ReactNode }
             </div>
           </div>
         </div>
-      )}
+      ) : null}
 
       <div className="flex-1 flex flex-col min-w-0">
         {/* Mobile topbar */}
